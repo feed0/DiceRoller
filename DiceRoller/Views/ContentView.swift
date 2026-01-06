0b0901757a94cbc401021ddc5d807492452cb9b9
feed0 <dogmas-gutters.07@icloud.com>
2026-01-06 11:57:51 -0300

    [DR-4] Create a dynamic display of dice
    - ContentView.swift:
        1. Added: title, diceHStack and remove and add buttons,

--- DiceRoller/Views/ContentView.swift
+++ DiceRoller/Views/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Properties
+    
+    @State private var diceCount = 2
+    
+    // MARK: - Body
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            title
+            diceHStack
+            
+            HStack {
+                removeDieButton
+                addDieButton
+            }
         }
         .padding()
+    }
+    
+    // MARK: - ViewBuilder
+    
+    private var title: some View {
+        Text("Dice Roller")
+            .font(.largeTitle
+                .lowercaseSmallCaps()
+                .bold()
+                .monospaced())
+    }
+    
+    private var diceHStack: some View {
+        HStack {
+            ForEach(1...diceCount, id: \.description) { _ in
+                DiceView()
+            }
+        }
+    }
+
+    // MARK: Buttons
+    
+    private var removeDieButton: some View {
+        Button("Remove Die") {
+            withAnimation {
+                diceCount -= 1
+            }
+        }
+        .disabled(diceCount == 1)
+    }
+    
+    private var addDieButton: some View {
+        Button("Add Die") {
+            withAnimation {
+                diceCount += 1
+            }
+        }
+        .disabled(diceCount == 3)
     }
 }
 
