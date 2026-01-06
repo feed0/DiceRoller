45438d6e58279543850aff21612b3b24621b5812
feed0 <dogmas-gutters.07@icloud.com>
2026-01-06 11:32:24 -0300

    [DR-3] Use state to update a view
    - DiceView.swift:
        1. Modified: numberOfPips is now a State var
        2. Added: withAnimation transition

--- DiceRoller/Views/Components/DiceView.swift
+++ DiceRoller/Views/Components/DiceView.swift
@@ -11,8 +11,9 @@ struct DiceView: View {
     
     // MARK: - Properties
     
-    var numberOfPips = 1
+    @State private var numberOfPips = 1
     
+    // MARK: Constants
     private let maxSideSize: CGFloat = 150
     private let rangeOfPips: ClosedRange<Int> = 1...6
     
@@ -20,15 +21,27 @@ struct DiceView: View {
     
     public var body: some View {
         VStack {
-            Image(systemName: "die.face.\(numberOfPips)")
-                .resizable()
-                .frame(maxWidth: maxSideSize,
-                       maxHeight: maxSideSize)
-            
-            Button("Roll") {
+            dieImage
+            rollTheDieButton
+        }
+    }
+    
+    // MARK: - ViewBuilder
+    
+    private var dieImage: some View {
+        Image(systemName: "die.face.\(numberOfPips)")
+            .resizable()
+            .frame(maxWidth: maxSideSize,
+                   maxHeight: maxSideSize)
+    }
+    
+    private var rollTheDieButton: some View {
+        Button("Roll") {
+            withAnimation {
                 numberOfPips = Int.random(in: rangeOfPips)
             }
         }
+        .buttonStyle(.bordered)
     }
 }
 
